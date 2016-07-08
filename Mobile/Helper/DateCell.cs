50299abdd67bad0f73fb8f440a2e31e78dcfd8c2
Victor Teixeira <vyctorbh@gmail.com>
2016-07-08 09:43:12 -0300

    * DateCell.cs: Form Projeção - 40% pronto.
    * Mobile.csproj:
    * SimulateAddPage.xaml:
    * SimulateAddPage.xaml.cs:

---
+++ Mobile/Helper/DateCell.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Mobile
+{
+	public class DateCell : ViewCell
+	{
+		private readonly DatePicker but;
+
+		public DateCell()
+		{
+			but = new DatePicker()
+			{
+				Format = "dd/MM/yyyy",
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand
+			};
+			var layout = new StackLayout()
+			{
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				Children = {
+					but
+				}
+			};
+
+			this.View = layout;
+		}
+
+		public DatePicker ButtonControl
+		{
+			get
+			{
+				return but;
+			}
+		}
+	}
+
+}
+
